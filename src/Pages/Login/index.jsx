80e32e3a1cf229580a949941ea6ae7ae9595b1d1
syntistx <syed.talha.khalid@gmail.com>
2023-12-03 21:08:05 -0500

    updated login ui

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,10 +1,22 @@
-import { Box, Button, TextField } from "@mui/material";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { signIn } from "../../api/auth";
 import { toast } from "react-toastify";
 import { useAuth } from "../../component/AuthContext";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  AppBar,
+  Toolbar,
+  Grid,
+  Card,
+  CardContent,
+  Container,
+  Paper,
+} from "@mui/material";
 
 export const Login = () => {
   const { login } = useAuth();
@@ -19,30 +31,55 @@ export const Login = () => {
       })
       .catch((err) => toast.error(err?.response?.data?.message));
   };
+
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-      mt={5}
-    >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField sx={{ mr: 5 }} label="Username" {...register("username")} />
-        <TextField label="Password" {...register("password")} type="password" />
-        <Box display="flex" justifyContent="center" alignItems="center" mt={5}>
-          <Button sx={{ mr: 3 }} variant="contained" type="submit">
-            Login
-          </Button>
-          <Button
-            onClick={() => navigate("/register")}
-            variant="contained"
-            type="submit"
-          >
-            Register
-          </Button>
-        </Box>
-      </form>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Medi-eKart
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm">
+        <Paper elevation={3} sx={{ padding: 3, marginTop: 4 }}>
+          <Typography variant="h4" align="center" gutterBottom>
+            Medi-eKart Login
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              fullWidth
+              margin="normal"
+              label="Username"
+              {...register("username")}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              label="Password"
+              {...register("password")}
+              type="password"
+            />
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              mt={3}
+            >
+              <Button variant="contained" color="primary" type="submit" sx={{ mr: 3 }}>
+                Login
+              </Button>
+              <Button
+                onClick={() => navigate("/register")}
+                variant="contained"
+                color="secondary"
+              >
+                Register
+              </Button>
+            </Box>
+          </form>
+        </Paper>
+      </Container>
     </Box>
   );
 };
